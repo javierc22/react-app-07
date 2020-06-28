d49465d4c40adb1b53f127bdfd2d7a8853153832
javierc22 <javier.ec.22@gmail.com>
2020-06-28 19:45:54 -0400

    Creando el Header y contenedor principal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h2>Noticias</h2>
+    <Fragment>
+      <Header 
+        titulo="Buscador de Noticias"
+      />
+
+      <div className="container white">
+        <h1>Formulario</h1>
+      </div>
+    </Fragment>
   );
 }
 
