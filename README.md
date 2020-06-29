a80f58064b551ad220d12ecea7d1262ceb5015e3
javierc22 <javier.ec.22@gmail.com>
2020-06-28 22:55:14 -0400

    Consultando la API y leyendo los resultados

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+
+
+
+* API: https://newsapi.org/
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
@@ -6,6 +6,19 @@ function App() {
 
   // Definir la categoría y noticias
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const apiKey = "5ae28db4634644c9b6aa80338868bca2";
+      const url = `http://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${apiKey}`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      guardarNoticias(noticias.articles);
+    }
+    consultarAPI();
+  }, [categoria])
 
   return (
     <Fragment>
