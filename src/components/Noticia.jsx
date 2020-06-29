b337a112e8d2b4ff7e1859d8bc2722e8b862cf8c
javierc22 <javier.ec.22@gmail.com>
2020-06-29 15:23:58 -0400

    Documentando la API

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Noticia = ({noticia}) => {
   // Extraer los datos
@@ -33,6 +34,10 @@ const Noticia = ({noticia}) => {
       </div>
     </div>
   );
+}
+
+Noticia.propTypes = {
+  noticia: PropTypes.object.isRequired
 }
  
 export default Noticia;
